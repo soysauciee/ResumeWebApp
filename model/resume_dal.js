12f6ac986d0d1f0bc6e409ba5a89529a3428994a
SoySaucE <soysauciee@gmail.com>
2016-12-03 03:21:24 -0800

    12/2/16

--- model/resume_dal.js
+++ model/resume_dal.js
@@ -4,8 +4,9 @@ var db  = require('./db_connection.js');
 /* DATABASE CONFIGURATION */
 var connection = mysql.createConnection(db.config);
 
+
 exports.getAll = function(callback) {
-    var query = 'SELECT * FROM resume;';
+    var query = 'SELECT * FROM resume_view;';
 
     connection.query(query, function(err, result) {
         callback(err, result);
@@ -13,10 +14,39 @@ exports.getAll = function(callback) {
 };
 
 exports.getById = function(resume_id, callback) {
-    var query = 'SELECT * FROM resume WHERE resume_id = ?';
+    var query = 'SELECT * FROM resume_view WHERE resume_id = ?';
     var queryData = [resume_id];
 
     connection.query(query, queryData, function(err, result) {
         callback(err, result);
     });
 };
+
+
+
+
+
+
+exports.insert = function(params, callback) {
+    var query = 'INSERT INTO resume (resume_name, resume_id) VALUES (?, ?)';
+
+    // the question marks in the sql query above will be replaced by the values of the
+    // the data in queryData
+    var queryData = [params.resume_name, params.address_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+}
+
+exports.delete = function(resume_id, callback) {
+    var query = 'DELETE FROM resume WHERE resume_id = ?';
+    var queryData = [resume_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+};
+
