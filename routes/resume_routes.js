0190bf25c1cacfb68a2f43d9069ce6f9e9575e33
SoySaucE <soysauciee@gmail.com>
2016-12-04 16:21:17 -0800

    12/3/16

--- routes/resume_routes.js
+++ routes/resume_routes.js
@@ -33,6 +33,25 @@ router.get('/', function(req, res){
     }
 });
 
+// Return the add a new school form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    address_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('resume/resumeAdd', {'address': result});
+        }
+    });
+});
+
+
+
+
+
+
+
 
 
 
